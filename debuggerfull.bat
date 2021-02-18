877eba4d506a45b3927ec84ea6b4f9616d311923
roey-coding <roey.eshkar@gmail.com>
2021-02-18 13:33:21 +0200

    with working dir

--- debuggerfull.bat
+++ debuggerfull.bat
@@ -1,5 +1,5 @@
 nasm.exe optimized_takeover_no_nrg_85.asm
 nasm.exe optimized_takeover_nrg_85.asm
-copy optimized_takeover_nrg_85 run_zombies_offline\corewars8086-4.0.2\survivors\nrg1
-copy optimized_takeover_no_nrg_85 run_zombies_offline\corewars8086-4.0.2\survivors\nrg2
-java -cp run_zombies_offline\corewars8086-4.0.2\lib\debugger.jar;run_zombies_offline\corewars8086-4.0.2\lib\* il.co.codeguru.corewars8086.CoreWarsEngine
+copy optimized_takeover_nrg_85 survivors\nrg1
+copy optimized_takeover_no_nrg_85 survivors\nrg2
+java -cp corewars8086-4.0.2\lib\debugger.jar;corewars8086-4.0.2\lib\* il.co.codeguru.corewars8086.CoreWarsEngine
