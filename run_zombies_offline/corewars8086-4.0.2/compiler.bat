710668699d13aa673f73b840d3e6049309e349b0
ILAY GUSSARSKY <ILAY.GUSSARSKY@wdc.com>
2021-02-18 10:56:53 +0200

    added run survivours offline. change .bat file to the correct directory and then it will work. Have java.

---
+++ run_zombies_offline/corewars8086-4.0.2/compiler.bat
@@ -0,0 +1,4 @@
+"C:\Users\7329934\Documents\Ilay Code\CodeGuru\codeguru-extreme-master\nasm\nasm.exe" "C:\Users\7329934\Documents\Ilay Code\CodeGuru\codeguru-extreme-master\nasm\optimized_takeover_no_nrg_85.asm"
+"C:\Users\7329934\Documents\Ilay Code\CodeGuru\codeguru-extreme-master\nasm\nasm.exe" "C:\Users\7329934\Documents\Ilay Code\CodeGuru\codeguru-extreme-master\nasm\optimized_takeover_nrg_85.asm"
+move "C:\Users\7329934\Documents\Ilay Code\CodeGuru\codeguru-extreme-master\nasm\optimized_takeover_nrg_85" "C:\Users\7329934\Documents\Ilay Code\CodeGuru\corewars8086-4.0.2\survivors\nrg1"
+move "C:\Users\7329934\Documents\Ilay Code\CodeGuru\codeguru-extreme-master\nasm\optimized_takeover_no_nrg_85" "C:\Users\7329934\Documents\Ilay Code\CodeGuru\corewars8086-4.0.2\survivors\nrg2"
